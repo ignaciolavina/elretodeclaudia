d140f9c6e3182712135302c140a8a6db9ec161f0
Ignacio.lavina <lavina.ignacio@gmail.com>
2026-04-19 10:53:58 +0200

    Included EmailJS service

--- src/config/emailjs.js
+++ src/config/emailjs.js
@@ -2,14 +2,14 @@
 // Sigue las instrucciones en EMAILJS_SETUP.md para obtener estos valores
 export const EMAILJS_CONFIG = {
   // En EmailJS > Account > General: "Public Key"
-  publicKey: 'REEMPLAZA_CON_TU_PUBLIC_KEY',
+  publicKey: "lI1MMZGRdjdKBkcTO",
 
   // En EmailJS > Email Services: el ID del servicio Gmail conectado
-  serviceId: 'REEMPLAZA_CON_TU_SERVICE_ID',
+  serviceId: "service_sbyc4fn",
 
   // En EmailJS > Email Templates: ID de la plantilla de notificación (llega a Claudia)
-  notificationTemplateId: 'REEMPLAZA_CON_TEMPLATE_NOTIFICACION_ID',
+  notificationTemplateId: "template_ma8f7xw",
 
   // En EmailJS > Email Templates: ID de la plantilla de confirmación (llega al remitente)
-  confirmationTemplateId: 'REEMPLAZA_CON_TEMPLATE_CONFIRMACION_ID',
-}
+  confirmationTemplateId: "template_24tze3p",
+};
